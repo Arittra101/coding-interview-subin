f8f7d4613edc905e2c6ccc217999380a8dc2156c
Arittra101 <avishek51775@gmail.com>
2026-06-20 14:32:24 +0600

    add interesting problem section

---
+++ Some Interesting Problems/3614. Process String with Special Operations II.cpp
@@ -0,0 +1,67 @@
+class Solution
+{
+public:
+    char processStr(string s, long long k)
+    {
+        long long int l = 0;
+        for (auto it : s)
+        {
+            if (it == '*')
+            {
+                if (l)
+                    l -= 1;
+            }
+            else if (it == '#')
+            {
+                l *= 2;
+            }
+            else if (it == '%')
+            {
+                continue;
+            }
+            else
+            {
+                l += 1;
+            }
+        }
+
+        if (k >= l)
+        {
+            return '.';
+        }
+
+        for (int i = s.size() - 1; i >= 0; i--)
+        {
+            char c = s[i];
+
+            if (c == '*')
+            {
+                l++;
+            }
+
+            else if (c == '#')
+            {
+                long long half = l / 2;
+                if (k >= half)
+                {
+                    k = (k - half);
+                }
+                l = half;
+            }
+
+            else if (c == '%')
+            {
+                k = l - k - 1;
+            }
+            else
+            {
+                if (k == l - 1)
+                    return c;
+                l--;
+            }
+        }
+        return '.';
+    }
+};
+
+// https://leetcode.com/problems/process-string-with-special-operations-ii/?envType=daily-question&envId=2026-06-17
